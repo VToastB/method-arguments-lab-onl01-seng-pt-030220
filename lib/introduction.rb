d78e53c6d33e111cb0300511a3c13b5e8a521a70
frank meyer <vtoastb@gmail.com>
2020-03-06 20:29:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(name)
+def (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name,language)
+def (hunter,ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
